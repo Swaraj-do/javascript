8732e9e089ffedc2721b4e9ad0db6486ab284015
Swaraj <swarajmohanty234@gmail.com>
2025-07-23 18:07:23 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using nodejs , not browser
+
+console.log(3
+    +
+     3) // code readability should be high 
+
+console.log("swaraj")
+
+let name = "swaraj"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// these are primitive datatypes.
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
