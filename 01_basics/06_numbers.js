093d260f352f913fcedfb55e001e8d0c4b71bce1
Swaraj <swarajmohanty234@gmail.com>
2025-07-25 13:54:31 +0000

    numbers

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,13 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherName = 23.8976
+
+console.log(otherName.toPrecision(3));
+ 
